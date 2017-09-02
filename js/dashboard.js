37ff7e30c56c76eab8a41f3eed0b1d37bf62e439
Ron Wendt <rwendt@LJFD.LOCAL>
2017-09-02 16:49:02 -0500

    One upcoming shift

--- js/dashboard.js
+++ js/dashboard.js
@@ -7,15 +7,15 @@ var LJFDSchedules = {} ;
 $(document).ready(function()
 {
     /* LOAD LJFD MEMBERS */
-	$.getJSON('php/membersLJFD.php5', function(data) 
+	$.getJSON('php/membersLJFD.php5', function(data)
 	{
 		LJFDMembers = data;
 
         /* LOAD LJFD SCHEDULES */
-		$.getJSON('php/schedulesLJFD.php5', function(data) 
+		$.getJSON('php/schedulesLJFD.php5', function(data)
 		{
 			LJFDSchedules = data;
-			
+
 			/* LOAD LJFD DASHBOARD */
         	loadDashboard();
         });
@@ -28,7 +28,7 @@ $(document).ready(function()
 /* -------------------------------------------------------------------------- */
 /* -------------------------------------------------------------------------- */
 
-function loadDashboard() 
+function loadDashboard()
 {
     if ( !isMobile.any )
 	{
@@ -41,8 +41,8 @@ function loadDashboard()
 	    loadOnDuty('140');
 	    loadEvents();
 	    loadUpcoming('120');
-	    loadUpcoming('140'); 
-	} 
+	    loadUpcoming('140');
+	}
 	else
 	{
 	    loadDutyChief('DC');
@@ -53,7 +53,7 @@ function loadDashboard()
 	    loadUpcoming('140');
 	}
  }
- 
+
 
 /* -------------------------------------------------------------------------- */
 /* -------------------------------------------------------------------------- */
@@ -74,13 +74,13 @@ function loadUpcoming(station)
 		var currentBT = moment(now).format("YYYY-MM-DD") + 'T' + moment(now).format("HH:mm") + ':00Z' ;
 		var currentET = moment(now24).format("YYYY-MM-DD") + 'T' + moment(now24).format("HH:mm") + ':10Z' ;
 		moment.tz.setDefault("America/Chicago") ;
-		
+
 		/* TRAVERSE THROUGH UPCOMING SCHEDULES */
 		$.each(results.ranges.range,  function(key,range)
 		{
-			
+
 			upComing = '{' ;
-			
+
 			/* IDENTIFY STATION BEING STAFFED */
 			switch ( range.schedule["@attributes"].id )
 			{
@@ -101,23 +101,23 @@ function loadUpcoming(station)
 				    stationSchedule = 'Station UNK' ;
 			}
 			upComing = upComing + '"station" : "' + stationSchedule + '",' ;
-			
-		
+
+
 			/* GET START TIME */
 			tempBegin = range.begin ;
 			strBegin = tempBegin.substr(0,10) + ' ' + tempBegin.substr(11,5) ;
 			shiftBegin = moment.tz(strBegin,'UTC') ;
 			shiftBegin.tz('America/Chicago') ;
-			upComing = upComing + '"shift" : "' + shiftBegin.format('YYYY-MM-DD HH:mm') + '",' ; 
-			upComing = upComing + '"shiftTime" : "' + shiftBegin.format('HH:mm') + '-' ; 
-			
+			upComing = upComing + '"shift" : "' + shiftBegin.format('YYYY-MM-DD HH:mm') + '",' ;
+			upComing = upComing + '"shiftTime" : "' + shiftBegin.format('HH:mm') + '-' ;
+
 			/* GET END TIME */
 			tempEnd = range.end ;
 			strEnd = tempEnd.substr(0,10) + ' ' + tempEnd.substr(11,5) ;
 			shiftEnd = moment.tz(strEnd,'UTC') ;
 			shiftEnd.tz('America/Chicago') ;
-			upComing = upComing + shiftEnd.format('HH:mm') + '",' ; 
-			
+			upComing = upComing + shiftEnd.format('HH:mm') + '",' ;
+
 			/* IDENTIFY POSITION BEING STAFFED */
 			$.each(LJFDSchedules.schedules.schedule, function(key,schedule)
 			{
@@ -155,7 +155,7 @@ function loadUpcoming(station)
 					return false;
 				}
 			});
-			
+
 			/* IDENTIFY MEMBER ON DUTY */
 			$.each(LJFDMembers.members.member, function(key, member)
     		{
@@ -165,14 +165,14 @@ function loadUpcoming(station)
 		    		upComing = upComing + '"ems" : "' + member.attributes.attribute[3].value + '"' ;
 					return false;
 	    		}
-    		});			
-        
+    		});
+
 		    upComing = upComing + '}' ;
 			resource[i] = upComing ;
 			i = i + 1 ;
-			
+
 		});
-		
+
 		/* SORT TABLE AND PREP FOR DISPLAY */
 		resource.sort() ;
 		var upComingJSON = '{ "upComingStaff" : [' ;
@@ -185,31 +185,32 @@ function loadUpcoming(station)
 			upComingJSON = upComingJSON + resource[i];
 
 		}
-		upComingJSON = upComingJSON + ']}' ;		
+		upComingJSON = upComingJSON + ']}' ;
 		var upComingObj = JSON.parse(upComingJSON) ;
-		
+
 		/* DISPLAY UPCOMING STAFF */
 		var stationList ;
 		var shiftTitle ;
-		var shiftTitlePrev ;
+		var shiftTitlePrev='FIRST SHIFT' ;
+		var shiftCounter=0;
 
 		$.each(upComingObj.upComingStaff, function(key,upComingStaff)
 		{
 			if ( key == 0 )
-			{ 
-			   $(".upcoming"+station).html('<tr>'); 
-			   currentTime = upComingStaff.shiftTime.substring(0,5) ;	
+			{
+			   $(".upcoming"+station).html('<tr>');
+			   currentTime = upComingStaff.shiftTime.substring(0,5) ;
 			}
 			else
-			{ 
-			   $(".upcoming"+station).append('<tr>'); 
+			{
+			   $(".upcoming"+station).append('<tr>');
 			}
-			
+
 			/* DISPLAY SHIFT TITLE */
-			startTime = upComingStaff.shiftTime.substring(0,5) ;	
-			endTime = upComingStaff.shiftTime.substring(6) ;	
+			startTime = upComingStaff.shiftTime.substring(0,5) ;
+			endTime = upComingStaff.shiftTime.substring(6) ;
 			startHour = upComingStaff.shiftTime.substring(0,2) ;
-			
+
 			if (( startTime == currentTime ) || (( endTime.substring(3) != '00' ) && ( endTime.substring(3) != '30' )))
 			{
 			   shiftTitle = 'UNKNOWN' ;
@@ -231,26 +232,33 @@ function loadUpcoming(station)
 			      {
 			         shiftTitle = 'NIGHT SHIFT' ;
 			      }
-			   }	
-			}			
-			
+			   }
+			}
+
 			if ( shiftTitle != 'UNKNOWN' )
-			{		
-			    if ( shiftTitle != shiftTitlePrev ) 
-			    {
-			        shiftTitlePrev = shiftTitle ;
-				    $(".upcoming"+station).append('<td colspan="4" class="scheduleRow shiftName">' + shiftTitle + '</td>');
-		    	    $(".upcoming"+station).append('</tr><tr>');
-			    }
-			
-			    /* DISPLAY STAFF */
-			    $(".upcoming"+station).append('<td class="scheduleRow Position">' + upComingStaff.position + '</td>');
-			    $(".upcoming"+station).append('<td class="scheduleRow Name">' + upComingStaff.name + '</td>');
-			    $(".upcoming"+station).append('<td class="scheduleRow Rank">' + upComingStaff.shiftTime + '</td>');	    
-		        $(".upcoming"+station).append('</tr>');
+			{
+						if ( shiftTitle != shiftTitlePrev )
+				    {
+							shiftCounter = shiftCounter + 1 ;
+							if ( shiftCounter == 1 )
+							{
+				      	shiftTitlePrev = shiftTitle ;
+					    	$(".upcoming"+station).append('<td colspan="4" class="scheduleRow shiftName">' + shiftTitle + '</td>');
+			    	  	$(".upcoming"+station).append('</tr><tr>');
+							}
+				    }
+
+				    /* DISPLAY STAFF */
+						if ( shiftCounter == 1 )
+						{
+				    	$(".upcoming"+station).append('<td class="scheduleRow Position">' + upComingStaff.position + '</td>');
+				    	$(".upcoming"+station).append('<td class="scheduleRow Name">' + upComingStaff.name + '</td>');
+				    	$(".upcoming"+station).append('<td class="scheduleRow Rank">' + upComingStaff.shiftTime + '</td>');
+			      	$(".upcoming"+station).append('</tr>');
+						}
 			}
 		});
-		    
+
 	});
 }
 
@@ -265,9 +273,9 @@ function loadDutyChief(station)
 	$.getJSON("php/onDutyLJFD.php5?station="+station, function(results)
 	{
 
-		$(".onDuty"+station).html('<tr>'); 
+		$(".onDuty"+station).html('<tr>');
 		$(".onDuty"+station).append('<td class="scheduleRow Position PositionOD">DUTY CHIEF</td>');
-			
+
 		/* IDENTIFY MEMBER ON DUTY */
 		$.each(LJFDMembers.members.member, function(key, member)
     	{
@@ -276,10 +284,10 @@ function loadDutyChief(station)
 		         $(".onDuty"+station).append('<td class="scheduleRow Name NameOD">' + member.name + '</td>');
 				 return false;
 	    	 }
-    	});			
-        
+    	});
+
 		$(".onDuty"+station).append('</tr>');
-		    
+
 	});
 }
 
@@ -296,14 +304,14 @@ function loadOnDuty(station)
 		var i = 0 ;
 		var resource = new Array () ;
     	var onDuty ;
-		
+
 		if (results.ranges.range.length > 1 )
-		{ 
+		{
 		/* TRAVERSE THROUGH ON DUTY SCHEDULES */
 		$.each(results.ranges.range,  function(key,range)
 		{
 			onDuty = '{' ;
-			
+
 			/* IDENTIFY POSITION ON DUTY */
 			$.each(LJFDSchedules.schedules.schedule, function(key,schedule)
 			{
@@ -341,7 +349,7 @@ function loadOnDuty(station)
 					return false;
 				}
 			});
-			
+
 			/* IDENTIFY MEMBER ON DUTY */
 			$.each(LJFDMembers.members.member, function(key, member)
     		{
@@ -351,13 +359,13 @@ function loadOnDuty(station)
 		    		onDuty = onDuty + '"ems" : "' + member.attributes.attribute[3].value + '"' ;
 					return false;
 	    		}
-    		});			
-        
+    		});
+
 		    onDuty = onDuty + '}' ;
 			resource[i] = onDuty ;
 			i = i + 1 ;
 		});
-		
+
 		resource.sort() ;
 		var onDutyJSON = '{ "onDutyNow" : [' ;
 		for ( i=0; i<resource.length; i++ )
@@ -369,9 +377,9 @@ function loadOnDuty(station)
 			onDutyJSON = onDutyJSON + resource[i];
 		}
 		onDutyJSON = onDutyJSON + ']}' ;
-			
+
 		var onDutyObj = JSON.parse(onDutyJSON) ;
-		
+
 		$.each(onDutyObj.onDutyNow, function(key,onDutyNow)
 		{
 		    if ( key == 0 )
@@ -387,7 +395,7 @@ function loadOnDuty(station)
 		else
 		{
 
-			$(".onDuty"+station).html('<tr>'); 
+			$(".onDuty"+station).html('<tr>');
 			/* IDENTIFY POSITION ON DUTY */
 			$.each(LJFDSchedules.schedules.schedule, function(key,schedule)
 			{
@@ -404,7 +412,7 @@ function loadOnDuty(station)
 					return false;
 				}
 			});
-			
+
 			/* IDENTIFY MEMBER ON DUTY */
 			$.each(LJFDMembers.members.member, function(key, member)
     		{
@@ -414,11 +422,11 @@ function loadOnDuty(station)
 			        $(".onDuty"+station).append('<td class="scheduleRow Rank RankOD">' + member.attributes.attribute[3].value + '</td>');
 	    			return false;
 	    		}
-    		});			
-		
+    		});
+
 		    $(".onDuty"+station).append('</tr>');
-		
-		}    
+
+		}
 	});
 }
 
@@ -430,15 +438,13 @@ function loadOnDuty(station)
 
 function loadEvents ()
 {
-    $(".events").load("php/eventsToday.php5", 
+    $(".events").load("php/eventsToday.php5",
 	    function(responseTxt, statusTxt, xhr){
             if(statusTxt == "success")
 			{
 //			    alert("Successful Return");
-			}	
+			}
             if(statusTxt == "error")
                 alert("Error: " + xhr.status + ": " + xhr.statusText);
         });
 }
-
-
